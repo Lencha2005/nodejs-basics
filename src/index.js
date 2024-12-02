5c5aa272f0149c585065679749bc2d572d741e85
Lena_Bondar <lencha.2005@gmail.com>
2024-12-02 15:03:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import express from 'express';
 
-const app = express();
-
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
-    });
-  });
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
