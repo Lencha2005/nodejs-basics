ad25b1148faf160b1d81b05875e8d5e92c756eac
Lena_Bondar <lencha.2005@gmail.com>
2025-01-12 16:21:56 +0200

    add

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,9 +22,13 @@ export const SMTP = {
 };
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+// export const TEMPLATES_DIR = path.resolve('src', 'templates');
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+// OR
+// export const TEMP_UPLOAD_DIR = path.resolve('temp');
+// export const UPLOAD_DIR = path.resolve('uploads');
 
 export const CLOUDINARY = {
   CLOUD_NAME: 'CLOUD_NAME',
--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -4,6 +4,7 @@ import { TEMP_UPLOAD_DIR } from '../constants/index.js';
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, TEMP_UPLOAD_DIR);
+    // or destination: TEMP_UPLOAD_DIR;
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now();
